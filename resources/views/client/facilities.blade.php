312b3d7bcde487855f50baf3e32379ffeea00d7b
Aydil Joe <aydiljoe@gmail.com>
2016-09-07 17:18:32 +0800

    Completed the facilities front page

--- resources/views/client/facilities.blade.php
+++ resources/views/client/facilities.blade.php
@@ -3,4 +3,121 @@
 @section('title', 'Facilities')
 
     @section('content')
+    <div class="row">
+      <div class="col-lg-1">
+        <a href="#" style="color:black;font-size: 200%"><i class="fa fa-arrow-left"></i> <span>Back</span></a>
+
+      </div>
+
+      <div class="col-lg-10">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="box">
+                <div class="box-header" style="background-color: #dd4b39">
+
+                  <h3 class="box-title" style="font-size:500%;color: white">Facilities</h3>
+                </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="box  card card-img ">
+                              <!-- /.box-header -->
+                  <div class="box-body">
+
+
+
+                        <div class="img ">
+                          <img src="http://www.bonappetit.com/wp-content/uploads/2016/02/dropbox-cafeteria.jpg" alt="First slide">
+
+                          <div class="carousel-caption">
+                            Cafeteria
+                          </div>
+                        </div>
+
+
+
+
+                  </div>
+                  <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+              </div>
+            <div class="col-md-6">
+                  <div class="box  card card-img ">
+                                <!-- /.box-header -->
+                    <div class="box-body">
+
+
+
+                          <div class="img ">
+                            <img src="http://www.greenmachineatms.com/wp-content/uploads/2016/01/atmp.jpg" alt="First slide">
+
+                            <div class="carousel-caption">
+                              Cafeteria
+                            </div>
+                          </div>
+
+
+
+
+                    </div>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- /.box -->
+                </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="box  card card-img ">
+                              <!-- /.box-header -->
+                  <div class="box-body">
+
+
+
+                        <div class="img ">
+                          <img src="https://hague6185.files.wordpress.com/2013/08/kazan_mosque_kul_sharif_sky-1920x1440.jpg" alt="First slide">
+
+                          <div class="carousel-caption">
+                            Cafeteria
+                          </div>
+                        </div>
+
+
+
+
+                  </div>
+                  <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+              </div>
+            <div class="col-md-6">
+                  <div class="box  card card-img ">
+                                <!-- /.box-header -->
+                    <div class="box-body">
+
+
+
+                          <div class="img ">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Kuala_Lumpur_Malaysia_Pos-Malaysia-Headquarters-01.jpg" alt="First slide">
+
+                            <div class="carousel-caption">
+                              Cafeteria
+                            </div>
+                          </div>
+
+
+
+
+                    </div>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- /.box -->
+                </div>
+        </div>
+      </div>
+    </div>
+
     @endsection
