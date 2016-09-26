ec1a4050a3911edc8d9f8fd1fffb1bc12e344f6f
Mohd Zafri Zulkipli <zafrizulkipli@gmail.com>
2016-09-26 17:36:17 +0800

    fix register bug again

--- resources/views/client/request-institution.blade.php
+++ resources/views/client/request-institution.blade.php
@@ -3,6 +3,7 @@
 <div class="box box-primary">
     <div class="box-body">
       <table class='table table-striped table-bordered table-hover table-condensed'>
+        @if($all_ri != null)
         <thead>
           <tr>
             <th>Institution Name</th>
@@ -18,6 +19,7 @@
           @endforeach
         </tbody>
       </table>
+      @endif
         <form role=form method=post action="{{route('client.request.add.institution')}}" autocomplete="off">
             <div class=box-body>
                 {{Form::select('institution_id',$i,null,['placeholder' => 'Select an institution',
