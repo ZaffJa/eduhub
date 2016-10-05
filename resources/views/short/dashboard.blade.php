3d35f25229babb63c282f8911e1fd3ae73b68045
Aydil Joe <aydiljohari@gmail.com>
2016-10-05 11:51:26 +0800

    Fix font error

--- resources/views/short/dashboard.blade.php
+++ resources/views/short/dashboard.blade.php
@@ -16,7 +16,7 @@
               <h5 class="widget-user-desc">{{Auth::user()->short_provider->headline != null ? Auth::user()->short_provider->headline : ""}}</h5>
             </div>
             <div class="widget-user-image">
-              <img class="img-circle hidden-xs" src="/img/avatar/boy-512-02.png" alt="User Avatar">
+              <img class="img-circle hidden-xs" src="/img/avatar/boy-512-03.png" alt="User Avatar">
             </div>
             <div class="box-footer">
               <div class="row">
@@ -51,13 +51,13 @@
           </div>
           <!-- /.widget-user -->
         </div>
-    </div>
+</div>
     <div class="row">
       <div class="col-lg-6">
         <a href="{!! route('short.profile.view') !!} ">
           <div class="box-body">
 
-            <div class="demo-card-image mdl-card mdl-shadow--2dp" style="background-image: url('https://freeiconshop.com/files/edd/people-outline.png'); background-size: 40%; background-color:white;">
+            <div class="demo-card-image mdl-card mdl-shadow--2dp" style="background-image: url('/img/avatar/boy-512-03.png'); background-size: 40%; background-color:white;">
               <div class="mdl-card__title mdl-card--expand"  >
 
               </div>
