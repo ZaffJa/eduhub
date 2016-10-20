4fccd17d60da9cbe9d4b9aeef6ce2d97eee445c3
ahambali <a.hambali.joferi@gmail.com>
2016-10-20 09:45:55 +0800

    Edit sidemenu profile pic

--- resources/views/client/layout/app.blade.php
+++ resources/views/client/layout/app.blade.php
@@ -146,7 +146,7 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel">
                     <div class="pull-left image">
-                        <img src="/img/logo/LOGO-U.svg" class="img-circle" alt="User Image">
+                        <img src="../img/{{isset($profilePic) ? $profilePic->path : ''}}" onerror="this.onerror=null;this.src='/img/avatar/picture.png'" class="img-circle" alt="User Image">
                     </div>
                     <div class="pull-left info">
 
