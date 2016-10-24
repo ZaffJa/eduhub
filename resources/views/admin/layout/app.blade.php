cab893b51ffc4b0ae8f2d41dda6e3f80156ba385
Mohd Zafri Zulkipli <zafrizulkipli@gmail.com>
2016-10-24 11:17:21 +0800

    Add delete institution for admin

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -210,10 +210,10 @@
                                     </ul>
                                 </div>
                                 @endif
-                                @if (isset($status))
+                                @if (session('status') != null)
                                 <div class="alert alert-success alert-dismissible">
                                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                                    <h4><i class="icon fa fa-check"></i> Success!</h4> {{ $status }}
+                                    <h4><i class="icon fa fa-check"></i> Success!</h4> {{ session('status') }}
                                 </div>
                                 @endif
                             </div>
