75f3b3ac49aceb63fabc7d489af4723c69500b21
Mohd Zafri Zulkipli <zafrizulkipli@gmail.com>
2016-11-07 00:50:07 +0800

    Create migrations for spm requirements

---
+++ app/Models/Student/SpmRequirementCourse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Student;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\AuditingTrait;
+
+class SpmRequirementCourse extends Model
+{
+    use AuditingTrait, SoftDeletes;
+    
+    protected $guarded = [];
+}
