94bb6f1b8bb71f401dec466ad57cd61a2dc9f9e5
Aydil Joe <aydiljohari@gmail.com>
2016-11-23 12:48:37 +0800

    new chart for displaying graph

--- resources/views/student/layout/app.blade.php
+++ resources/views/student/layout/app.blade.php
@@ -17,18 +17,6 @@
 	<!-- Canonical SEO -->
 	<link rel="canonical" href="http://www.creative-tim.com/product/material-dashboard" />
 
-	<!--  Social tags      -->
-	<meta name="keywords" content="material dashboard, bootstrap material admin, bootstrap material dashboard, material design admin, material design, creative tim, html dashboard, html css dashboard, web dashboard, freebie, free bootstrap dashboard, css3 dashboard, bootstrap admin, bootstrap dashboard, frontend, responsive bootstrap dashboard">
-
-	<meta name="description" content="Material Dashboard is a Free Material Bootstrap Admin with a fresh, new design inspired by Google's Material Design.">
-
-	<!-- Schema.org markup for Google+ -->
-	<meta itemprop="name" content="Material Dashboard by Creative Tim | Free Material Bootstrap Admin">
-	<meta itemprop="description" content="Material Dashboard is a Free Material Bootstrap Admin with a fresh, new design inspired by Google's Material Design.">
-	<meta itemprop="image" content="../../../s3.amazonaws.com/creativetim_bucket/products/50/opt_md_thumbnail.jpg">
-
-
-
 	<!-- Bootstrap core CSS     -->
 	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
 
@@ -40,18 +28,18 @@
 
 	<!--     Fonts and icons     -->
 	<link href="../../../assets/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'> @yield('header-css')
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
 
-<!--   Core JS Files   -->
+	<!--   Core JS Files   -->
 	<script src="/assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
 	<script src="/assets/js/bootstrap.min.js" type="text/javascript"></script>
 	<script src="/assets/js/material.min.js" type="text/javascript"></script>
 
 	<!--  Charts Plugin -->
+	<script src="https://code.highcharts.com/highcharts.js"></script>
+	<script src="https://code.highcharts.com/highcharts-more.js"></script>
+	<script src="https://code.highcharts.com/modules/exporting.js"></script>
 
-	<script src="/assets/js/chartist.min.js"></script>
-	<script src="/assets/js/graph.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.11.8/TweenMax.min.js"></script>
 
 	<!--  Notifications Plugin    -->
 	<script src="/assets/js/bootstrap-notify.js"></script>
@@ -66,6 +54,7 @@
 	<script src="/assets/js/jquery.sharrre.js"></script>
 
 	<script src="/assets/js/demo.js"></script>
+	 @yield('header-css')
 </head>
 
 <body>
@@ -301,8 +290,8 @@
 		});
 	</script>
 
-  <script src="/assets/js/graph.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.11.8/TweenMax.min.js"></script>
+	<script src="/assets/js/graph.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.11.8/TweenMax.min.js"></script>
 
 </body>
 <!-- Mirrored from demos.creative-tim.com/material-dashboard/examples/dashboard.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 27 Oct 2016 07:41:19 GMT -->
