b55c9238fa5e47639a396edc206f90395332f10e
ahambali <a.hambali.joferi@gmail.com>
2016-12-06 11:19:26 +0800

    Duplicated code removed

--- resources/views/school/main/dashboard.blade.php
+++ resources/views/school/main/dashboard.blade.php
@@ -50,7 +50,6 @@
         #target {
           width: 345px;
         }
-    </style>
 
         .ui-widget {
             margin-top: 1%;
@@ -180,7 +179,6 @@
                         <h2 class="title"><b>School Location</b></h2>
                     </div>
                     <div class="card-content">
-                      <input id="pac-input" class="controls" type="text" placeholder="Search Box" oninput="searchName()" onchange="searchName()">
                         <div class="text-center">
                             <form class="navbar-form " role="search">
                                 <div class="form-group is-empty">
@@ -269,71 +267,4 @@
 
     }
 </script>
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCgdkEKOxECZPSbpr7MvPZMLH7sBGeIbV8&libraries=places&callback=initMap">
-    </script>
-    <script>
-        var map;
-        var schoolMarker = [];
-        var schoolInfo = [];
-        var schoolData = [];
-
-        function searchName() {
-            search = document.getElementById("pac-input").value;
-            for (i = 0; i < schoolData.length; i++) {
-                if ( schoolData[i][0].includes(search))
-                {
-                    map.panTo(schoolData[i][1]);
-                }
-            }
-        }
-
-        function initMap() {
-            var pastLocation = new google.maps.LatLng(
-                parseFloat("{{isset($location->latitude) ? $location->latitude : 1.5300076438874903}}").toFixed(6),
-                parseFloat("{{isset($location->longtitude) ? $location->longtitude : 103.765869140625}}").toFixed(6)
-            );
-
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: pastLocation,
-                zoom: 11,
-            });
-
-            @foreach($schoolLocation as $school)
-            schoolData.push(
-                new Array (
-                    "{{$school->school->name}}",
-                    position = new google.maps.LatLng
-                    (
-                        parseFloat("{{$school->latitude}}").toFixed(6),
-                        parseFloat("{{$school->longtitude}}").toFixed(6)
-                    )
-                )
-            );
-                    @endforeach
-
-                    @foreach ($schoolLocation as $key=>$school)
-            var schoolPosition = new google.maps.LatLng(
-                parseFloat("{{$school->latitude}}").toFixed(6),
-                parseFloat("{{$school->longtitude}}").toFixed(6)
-                );
-
-            schoolMarker[{{$key}}] = new google.maps.Marker({
-                position : schoolPosition,
-                map: map,
-                title: 'School location',
-                icon: '/img/logo/map-00.png'
-            });
-
-            schoolInfo[{{$key}}] =  new google.maps.InfoWindow({
-                content: "{{$school->school->name}}"
-            })
-
-            schoolMarker[{{$key}}].addListener('click', function() {
-                schoolInfo[{{$key}}].open(map, schoolMarker[{{$key}}])
-            });
-            @endforeach
-
-        }
-    </script>
 @endsection
