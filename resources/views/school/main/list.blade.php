a74eed4c848d8b45d275e9be68b461b791f97ce8
Aydil Joe <aydiljohari@gmail.com>
2016-12-21 10:45:02 +0800

    Follow institution look with overflow

--- resources/views/school/main/list.blade.php
+++ resources/views/school/main/list.blade.php
@@ -13,7 +13,8 @@
         #school_state {
             border: 1px solid black;
         }
-        #school_streams{
+
+        #school_streams {
             border: 1px solid black;
         }
     </style>
@@ -24,12 +25,13 @@
                 <div class="col-md-10 col-md-offset-1 alert alert-default">
                     <h3><strong>Carian Sekolah</strong></h3>
                     <form action="{{ action('School\SchoolController@postSearch') }}" method="post">
-                    <div class="row">
+                        <div class="row">
                             <div class="col-md-8 col-lg-8 col-sm-10">
-                                    {{Form::text('name',null,['class'=>'form-control input-box','placeholder'=>'Cari sekolah','id'=>'cariSekolah'])}}
+                                {{Form::text('name',null,['class'=>'form-control input-box','placeholder'=>'Cari sekolah','id'=>'cariSekolah'])}}
                             </div>
                             <div class="col-lg-2 col-md-2 col-sm-2">
-                                <button type="submit" class="btn btn-info btn-sm" style="top:25px;left:0px;"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                <button type="submit" class="btn btn-info btn-sm" style="top:25px;left:0px;"><i
+                                            class="fa fa-search" aria-hidden="true"></i></button>
                             </div>
                         </div>
 
@@ -50,90 +52,87 @@
                         </div>
                     </form>
 
-                    <form action="{{ action('School\SchoolController@filterStream') }}" method="post">
-                        <div class="form-group">
-                            {{ csrf_field() }}
-                            {{ Form::select('school_stream',$streams, !empty($streams) ? $streams: null,['class'=>'form-control','id'=>'school_streams','placeholder'=>'Aliran    ']) }}
-                        </div>
-                    </form>
+                    {{--<form action="{{ action('School\SchoolController@filterStream') }}" method="post">--}}
+                        {{--<div class="form-group">--}}
+                            {{--{{ csrf_field() }}--}}
+                            {{--{{ Form::select('school_stream',$streams, !empty($streams) ? $streams: null,['class'=>'form-control','id'=>'school_streams','placeholder'=>'Aliran    ']) }}--}}
+                        {{--</div>--}}
+                    {{--</form>--}}
                 </div>
             </div>
             <div class="row">
-            <div class="col-md-10 col-md-offset-1  alert alert-default alert-dismissible">
-
+                <div class="col-md-10 col-md-offset-1  alert alert-default alert-dismissible">
                     <div class="text-center">
-
                         <div class=" text-center">
                             <div class="col-lg-12">
                                 <h2 class="title">Ujian</h2>
-                            <img src="/img/icon/personality.png" style="height: 70%; width: 70%"/>
+                                <img src="/img/icon/personality.png" style="height: 70%; width: 70%"/>
                                 <h2 class="title">Personaliti</h2>
                             </div>
                             <div class="col-lg-12">
-
-                            <p>Ambil ujian personaliti anda.</p>
-                            <a href="{{action('School\PublicPersonalityController@set1')}}" class="btn btn-info btn-round text-wrap" >Ambil Ujian</a>
-
+                                <p>Ambil ujian personaliti anda.</p>
+                                <a href="{{action('School\PublicPersonalityController@set1')}}"
+                                   class="btn btn-info btn-round text-wrap">Ambil Ujian</a>
                             </div>
                         </div>
-
                     </div>
-
+                </div>
             </div>
-
-            </div>
-
         </div>
         <div class="col-lg-7 ">
-
-
-                    <div class="col-lg-12"  >
-
-                        <h4 class=" text-center school-title">Semua Sekolah</h4>
-
-                    </div>
+            <div class="col-lg-12">
+                @if(isset($type))
+                    <h4 class=" text-center school-title">Carian mengikut jenis Sekolah</h4>
+                @elseif(isset($school_state))
+                    <h4 class=" text-center school-title">Carian sekolah di negeri {{ $school_state or '...' }}</h4>
+                @else
+                    <h4 class=" text-center school-title">Semua Sekolah</h4>
+                @endif
+            </div>
 
             @if(count($schools) > 0)
 
-<div class="row" >
+                <div class="row">
                     @foreach($schools as $school)
 
-                        <div class="alert alert-default alert-dismissible col-md-4 col-sm-12 col-xs-12 col-lg-4 card-size text-center" id="cardSchool" >
+                        <div class="alert alert-default alert-dismissible col-md-4 col-sm-12 col-xs-12 col-lg-4 card-size text-center"
+                             id="cardSchool">
 
 
-                                <a href="{{ action('School\SchoolController@viewSchool',$school->slug) }}"  >
-                                    <strong ><span class="truncate" style="max-width: 100%;">{{ $school->name }}</span></strong> <br>
-                                    <i class="fa fa-map-marker " aria-hidden="true"></i> {{ $school->state }} <br>
-                                    <i class="fa fa-phone fa-fw" aria-hidden="true"></i> +60{{ $school->telephone }} <hr>
-                                    {{ $school->typeSchool->name }}
-                                </a>
+                            <a href="{{ action('School\SchoolController@viewSchool',$school->slug) }}">
+                                <strong><span class="truncate" style="max-width: 100%;">{{ $school->name }}</span></strong> <br>
+                                <i class="fa fa-map-marker " aria-hidden="true"></i> {{ $school->state }} <br>
+                                <i class="fa fa-phone fa-fw" aria-hidden="true"></i> +60{{ $school->telephone }}
+                                <hr>
+                                {{ $school->typeSchool->name }}
+                            </a>
 
 
-                                @if(auth()->user() !== null && auth()->user()->hasRole('school'))
-                                    <hr>
+                            @if(auth()->user() !== null && auth()->user()->hasRole('school'))
+                                <hr>
                                 <div class="card-footer">
                                     <button type="button" class="btn btn-danger btnDelete" data-href="{{ $school->id }}"
                                             style="margin: -10px; width: 100%; position: relative;  ">Delete
                                     </button>
                                 </div>
-                                @endif
+                            @endif
 
                         </div>
 
                     @endforeach
-</div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-3">
-                    {{ $schools->render()}}
                 </div>
-            </div>
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-3">
+                        {{ $schools->render()}}
+                    </div>
+                </div>
 
         </div>
 
 
-            @else
-                <h3> Tiada sekolah untuk carian ini</h3>
-            @endif
+        @else
+            <h3> Tiada sekolah untuk carian ini</h3>
+        @endif
 
 
     </div>
@@ -152,24 +151,24 @@
                     var jqxhr = $.get("/school/delete/" + close.data('href'), function () {
                         console.log("Sent");
                     })
-                            .done(function (result) {
+                        .done(function (result) {
 
-                                $.notify({
-                                    message: "<strong>Successfully deleted " + result + "</strong>"
-                                }, {
-                                    type: 'success'
-                                });
-
-                                close.closest('div').hide();
-                            })
-                            .fail(function () {
-                                $.notify({
-                                    message: "An error occurred"
-                                }, {
-                                    type: 'warning'
-                                });
-
+                            $.notify({
+                                message: "<strong>Successfully deleted " + result + "</strong>"
+                            }, {
+                                type: 'success'
                             });
+
+                            close.closest('div').hide();
+                        })
+                        .fail(function () {
+                            $.notify({
+                                message: "An error occurred"
+                            }, {
+                                type: 'warning'
+                            });
+
+                        });
                 }
             });
         });
@@ -192,31 +191,31 @@
         $('#cariSekolah').autocomplete({
             source: '{{ action('School\SchoolController@search') }}'
         });
-        $(window).on('load', function() {
-            if($(window).width() > 1000) {
+        $(window).on('load', function () {
+            if ($(window).width() > 1000) {
 
 
-                $('.card-size-xs').each(function(){            //iterates all elements having stick class
+                $('.card-size-xs').each(function () {            //iterates all elements having stick class
                     $(this).addClass('card-size');
                     $(this).removeClass('card-size-xs');//inside the callback the 'this' is the current html element. etc ...
                 });
-            }else{
-                $('.card-size').each(function(){            //iterates all elements having stick class
+            } else {
+                $('.card-size').each(function () {            //iterates all elements having stick class
                     $(this).addClass('card-size-xs');
                     $(this).removeClass('card-size');//inside the callback the 'this' is the current html element. etc ...
                 });
             }
         });
-        $(window).on('resize', function() {
-            if($(window).width() > 1000) {
+        $(window).on('resize', function () {
+            if ($(window).width() > 1000) {
 
 
-                $('.card-size-xs').each(function(){            //iterates all elements having stick class
+                $('.card-size-xs').each(function () {            //iterates all elements having stick class
                     $(this).addClass('card-size');
                     $(this).removeClass('card-size-xs');//inside the callback the 'this' is the current html element. etc ...
                 });
-            }else{
-                $('.card-size').each(function(){            //iterates all elements having stick class
+            } else {
+                $('.card-size').each(function () {            //iterates all elements having stick class
                     $(this).addClass('card-size-xs');
                     $(this).removeClass('card-size');//inside the callback the 'this' is the current html element. etc ...
                 });
